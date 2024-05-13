45ca13e2f5a3c3fc2a9de8ec54c95b6276217122
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-05-12 23:26:38 -0400

    Time: 10 ms (97.22%), Space: 12 MB (18.10%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -4,12 +4,15 @@ class Solution(object):
         :type text: str
         :rtype: int
         """
-        countText = Counter(text)
-        balloon = Counter("balloon")
+        # countText = Counter(text)
+        # balloon = Counter("balloon")
 
-        res = len(text)  # or float("inf")
-        for c in balloon:
-            res = min(res, countText[c] //balloon[c])
-        return res
+        # res = len(text)  # or float("inf")
+        # for c in balloon:
+        #     res = min(res, countText[c] // balloon[c])
+        # return res
+
+        return min(text.count('b'), text.count('a'), text.count('l') // 2, text.count('o') // 2, text.count('n'))
+
                 
         
