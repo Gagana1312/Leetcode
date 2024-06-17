0770da784442a555bae36d187a19bdb50b26e4ae
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-06-17 09:59:46 -0400

    Time: 177 ms (58.32%), Space: 11.9 MB (37.42%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -22,6 +22,22 @@ class Solution(object):
                 r+=1
         return sol
 
+    #     sol = 0
+    #     for i in range(len(s)):
+    #         l,r=i,i
+    #         sol += self.countPali(s, l , r)
+    #         l,r=i,i+1
+    #         sol += self.countPali(s, l , r)
+    #     return sol
+
+    # def countPali(self, s, l, r):
+    #     sol=0
+    #     while l>=0 and r<len(s) and s[l]==s[r]:
+    #         sol+=1
+    #         l-=1
+    #         r+=1
+    #     return sol
+
 
 
         
