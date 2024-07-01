58e26ee70a29775ed330ac60a8cb166be09db30d
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-07-01 11:22:49 -0400

    Time: 2 ms (86.94%), Space: 55.3 MB (58.66%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,44 +1,40 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // int[] res = new int[10];
-        // int prod = 1;
-        // for(int i =0 ; i< nums.length-1;i++){
-        //     if(i==0){
-        //         for(int j=i;j<nums.length-i;j++){
-        //             prod*=nums[j];
-        //         }
-        //     } else {
-        //         for(int j=i;j<nums.length-i;j++){
-        //             prod*=nums[j];
-        //         }
-        //         for (int j=0;j<i;j++){
-        //             prod*=nums[j];
-        //         }
+        //works
+        // int N = nums.length;
+        // int[] left_prod = new int[N];
+        // int[] right_prod = new int[N];
 
-        //     }
-        //     res.append(prod);
-        //     prod=1;
+        // int[] res = new int[N];
+
+        // left_prod[0] = 1;
+        // right_prod[N-1] =1;
+
+        // for (int i =1; i<N;i++){
+        //     left_prod[i]  = nums[i-1]* left_prod[i-1];
+        // }
+        // for (int i=N-2;i>=0;i--){
+        //     right_prod[i]  = nums[i+1] * right_prod[i+1];
         // }
 
+        // for(int i=0;i<N;i++){
+        //     res[i] = left_prod[i] * right_prod[i];
+        // }
+        // return res;
         int N = nums.length;
-        int[] left_prod = new int[N];
-        int[] right_prod = new int[N];
-
         int[] res = new int[N];
 
-        left_prod[0] = 1;
-        right_prod[N-1] =1;
+       res[0] = 1;
 
         for (int i =1; i<N;i++){
-            left_prod[i]  = nums[i-1]* left_prod[i-1];
+            res[i]  = nums[i-1]* res[i-1];
         }
-        for (int i=N-2;i>=0;i--){
-            right_prod[i]  = nums[i+1] * right_prod[i+1];
+        int temp =1;
+        for (int i=N-1;i>=0;i--){
+            res[i]  = res[i] * temp;
+            temp = temp * nums[i];
         }
 
-        for(int i=0;i<N;i++){
-            res[i] = left_prod[i] * right_prod[i];
-        }
         return res;
 
     }
