5c92655f5e4dea4cd1c0e53869161222da824eef
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-07-03 08:05:03 -0400

    Time: 5 ms (85.96%), Space: 44.5 MB (58.48%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,33 +1,32 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // HashSet<String> ls = new HashSet();
-        // int count =0;
-        // int left = 0;
 
+        // Works
+        // int maxlength = 0 ;
         // for(int i=0;i<s.length();i++){
-        //     while(int s[right]: ls){
-        //         ls.remove(s[left]);
-        //         left++;
+        //     StringBuilder currsub = new StringBuilder();
+        //     for (int j = i; j < s.length();j++){
+        //         if(currsub.indexOf(String.valueOf(s.charAt(j))) != -1 ){
+        //             break;
+        //         }
+        //         currsub.append(s.charAt(j));
+        //         maxlength = Math.max(maxlength,currsub.length());
+
         //     }
-        //     ls.add(s[right]);
-        //     count= math.max(count, right-left+1);
+
         // }
-        // return count;  
+        // return maxlength;
 
-        int maxlength = 0 ;
-        for(int i=0;i<s.length();i++){
-            StringBuilder currsub = new StringBuilder();
-            for (int j = i; j < s.length();j++){
-                if(currsub.indexOf(String.valueOf(s.charAt(j))) != -1 ){
-                    break;
-                }
-                currsub.append(s.charAt(j));
-                maxlength = Math.max(maxlength,currsub.length());
-
+        int maxlength =0;
+        HashMap<Character, Integer> visited = new HashMap<>();
+        for (int right = 0,left = 0; right< s.length();right++){
+            char currchar = s.charAt(right);
+            if(visited.containsKey(currchar) && visited.get(currchar) >= left){
+                left = visited.get(currchar)+1;
             }
-
+            maxlength = Math.max( maxlength, right-left+1);
+            visited.put(currchar,right);
         }
         return maxlength;
-
     }
 }
