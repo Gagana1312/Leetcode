87621ea94bb73a5187859d61a10c0a4ad4d44bb3
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-07-05 21:17:29 -0400

    Time: 2 ms (54.56%), Space: 41.7 MB (12.87%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -21,27 +21,5 @@ class Solution {
         return bracks.isEmpty();
 
 
-
-        // Stack<Character> brackets = new Stack<>();
-        // Map<Character, Character> bracketLookup = new HashMap<>(3);
-
-        // bracketLookup.put(')', '(');
-        // bracketLookup.put('}', '{');
-        // bracketLookup.put(']', '[');
-
-        // for (int i = 0; i < s.length(); i++) {
-        //     char c = s.charAt(i);
-        //     if (bracketLookup.containsKey(c)) {
-        //         if (!brackets.isEmpty() && bracketLookup.get(c).equals(brackets.peek())) {
-        //             brackets.pop();
-        //         } else {
-        //             return false;
-        //         }
-        //     } else {
-        //         brackets.push(c);
-        //     }
-        // }
-
-        // return brackets.isEmpty();
     }
 }
