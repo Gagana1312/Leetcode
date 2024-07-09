d4cf91526bdd2cc1460069514d39a93c3332c8bf
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-07-09 09:04:58 -0400

    Time: 24 ms (60.58%), Space: 11.9 MB (93.21%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -5,11 +5,30 @@ class Solution(object):
         :type target: int
         :rtype: bool
         """
-        for i in range(len(matrix)):
-            for j in range(len(matrix[i])):
-                if target in matrix[i]:
-                    return True
-        return False
+        #linear search
+        # for i in range(len(matrix)):
+        #     for j in range(len(matrix[i])):
+        #         if target in matrix[i]:
+        #             return True
+        # return False
+
+        # Binary search
+        if not matrix:
+            return False
+        rows, cols = len(matrix), len(matrix[0])
+        l, r = 0, rows*cols-1
+
+        while l<= r:
+            mid = r+(l-r)//2
+            midmat = matrix[mid //cols][mid%cols]
+
+            if midmat < target:
+                l = mid+1
+            elif midmat > target:
+                r= mid-1
+            else:
+                return True
+
 
 
 
