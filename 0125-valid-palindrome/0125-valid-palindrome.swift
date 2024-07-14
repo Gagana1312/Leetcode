baa6caba2a93e1a70ac1e0a63c6a6a3fdd45ea72
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-07-13 23:58:59 -0400

    Time: 18 ms (84.65%), Space: 18.1 MB (35.29%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.swift
+++ 0125-valid-palindrome/0125-valid-palindrome.swift
@@ -1,11 +1,30 @@
 class Solution {
     func isPalindrome(_ s: String) -> Bool {
+      var l = 0, r = s.count - 1
+      let sChars = Array(s.lowercased())
+      
+      while l < r {
+        while !sChars[l].isAlphanumeric && l < r {
+          l += 1
+        }
         
-        let cleanedString = s.replacingOccurrences(of: "[^a-zA-Z0-9]", with: "", options: .regularExpression).lowercased()
-        let reversedString = String(cleanedString.reversed())
-        return cleanedString == reversedString
-     
-    
+        while !sChars[r].isAlphanumeric && l < r {
+          r -= 1
+        }
         
+        if sChars[l] != sChars[r] {
+          return false
+        } else {
+          l += 1
+          r -= 1
+        }
+      }
+      return true
     }
+}
+
+extension Character {
+  var isAlphanumeric : Bool {
+    return isLetter || isNumber
+  }
 }
