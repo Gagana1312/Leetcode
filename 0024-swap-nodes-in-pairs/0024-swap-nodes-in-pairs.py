630737d005819af8ae3ed578160cd2e3155e1f6d
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-07-16 16:14:48 -0400

    Time: 32 ms (78.05%), Space: 16.5 MB (16.47%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+
+        while True:
+            Second = self.getKth(groupPrev, 2)
+            if not Second:
+                break
+            groupNext = Second.next
+
+            # reverse group
+            prev, curr = Second.next, groupPrev.next
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+
+            tmp = groupPrev.next
+            groupPrev.next = Second
+            groupPrev = tmp
+        return dummy.next
+
+    def getKth(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+        return curr
+
+
