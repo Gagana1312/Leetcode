2a2aee6f089e10b3dddc2ca8cadb69087aa58b6a
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-07-27 16:19:45 -0400

    Time: 100 ms (75.63%), Space: 15.7 MB (30.56%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.swift
@@ -0,0 +1,14 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        
+        var l=0
+        var r = nums.count-1
+        while l<=r{
+            let mid = r+(l-r)/2
+            guard nums[mid] != target else{return mid}
+            l = nums[mid] < target ? mid+1 : l
+            r = nums[mid] > target ? mid-1 : r
+        }
+        return -1
+    }
+}
