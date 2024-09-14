710fd213001dc74c1f503b5b959b77da76b2254e
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-09-14 15:27:59 -0600

    Time: 1831 ms (5.01%), Space: 25.9 MB (45.65%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,12 +1,31 @@
-class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        res =[0]*len(temperatures)
-        stack=[] #[temp., index]
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        # res=[0]*len(temperatures)
+        # l,r= 0,1
+        # while l<r and r<len(temperatures):
+        #     if temperatures[l]<temperatures[r]:
+        #         res[l]= r-l
+        #         l+=1
+        #         r=l+1
+        #     else:
+        #         r+=1
+        # return res
 
-        for i , t in enumerate(temperatures):
-            while stack and t>stack[-1][0]:
-                stackT, stackI = stack.pop()
-                res[stackI]= (i- stackI)
-            stack.append([t,i])
+
+
+        temp = temperatures
+        n = len(temperatures)
+        res=[0]*n
+        stack =[]
+
+        for i, t in enumerate(temp):
+            while stack and stack[-1][0]<t:
+                stack_t, stack_i = stack.pop()
+                res[stack_i] = i - stack_i
+            stack.append((t,i))
         return res
         
