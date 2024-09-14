aba26c7ed576b722af62df5d82dbe0862ed2279a
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-09-14 13:46:26 -0600

    Time: 78 ms (5.42%), Space: 13.8 MB (18.59%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,21 +1,27 @@
-class Solution:
-    def evalRPN(self, tokens: List[str]) -> int:
-        stack =[]
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        res = []
         for c in tokens:
-            if c == "+":
-                stack.append(stack.pop()+stack.pop())
-            elif c == "-":
-                a, b = stack.pop(), stack.pop()
-                stack.append(int(b-a))
-            elif c == "*":
-                stack.append(stack.pop()*stack.pop())
-            elif c == "/":
-                a, b = stack.pop(), stack.pop()
-                stack.append(int(b/a))
-            else:
-                stack.append(int(c))
-        return stack[0]
-
-
-
-        
+            if c.lstrip('-').isdigit(): 
+                res.append(int(c))  
+            elif c == "+" and len(res) >= 2:
+                i = res.pop()
+                j = res.pop()
+                res.append(j + i)
+            elif c == "-" and len(res) >= 2:
+                i = res.pop()
+                j = res.pop()
+                res.append(j - i)
+            elif c == "/" and len(res) >= 2:
+                i = res.pop()
+                j = res.pop()
+                res.append(int(float(j) / i))
+            elif c == "*" and len(res) >= 2:
+                i = res.pop()
+                j = res.pop()
+                res.append(j * i)
+        return res[0] 
