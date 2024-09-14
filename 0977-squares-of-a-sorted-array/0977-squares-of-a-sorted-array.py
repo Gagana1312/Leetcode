26913d3ccd2c83785859c1d46f4e0671d1f8f9ad
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-09-14 13:17:12 -0600

    Time: 271 ms (5.13%), Space: 13.7 MB (47.03%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -4,11 +4,25 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        res = []
-        for n in nums:
-            x= n*n
-            res.append(x)
-        res = sorted(res)
+        # res = []
+        # for n in nums:
+        #     x= n*n
+        #     res.append(x)
+        # res = sorted(res)
+        # return res
+
+        l,r = 0,len(nums)-1
+        res =[]
+        while l<=r:
+            if abs(nums[l])>abs(nums[r]):
+                res.append(nums[l] ** 2)
+                l+=1
+            else:
+                res.append(nums[r ]** 2)
+                r-=1
+        res.reverse()
         return res
+
+
 
         
