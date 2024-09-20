003f63a7a31f15c6b70eb7374e2dc93b60d6c397
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-09-20 10:39:00 -0600

    Time: 27 ms (12.41%), Space: 12.6 MB (22.76%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -1,27 +1,31 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res = []
-        q = collections.deque()
-        if root:
-            q.append(root)
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+        q = deque()
+        q.append(root)
+        ans=[]
         while q:
-            val = []
-            for i in range(len(q)):
+            level =[]
+            n = len(q)
+            for i in range(n):
                 node = q.popleft()
-                val.append(node.val)
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            res.append(val)
-        return res
-            
+                level.append(node.val)
+
+                if node.left: q.append(node.left)
+                if node.right: q.append(node.right)
+            ans.append(level)
+        return ans
 
 
-
+        
