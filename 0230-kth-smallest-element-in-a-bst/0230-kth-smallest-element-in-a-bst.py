69e777c4c57166d90e243796076b9ef6024c5fd8
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-09-20 13:42:40 -0600

    Time: 40 ms (14.94%), Space: 19.7 MB (11.10%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -1,22 +1,41 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def kthSmallest(self, root: TreeNode, k: int) -> int:
-        stack = []
-        curr = root
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
 
-        while stack or curr:
-            while curr:
-                stack.append(curr)
-                curr = curr.left
-            curr = stack.pop()
-            k -= 1
-            if k == 0:
-                return curr.val
-            curr = curr.right
+        q = deque()
+        res = []
+        ans = []
+        value = 0
+        q.append(root)
+        res.append(root.val)
+        while q:
+            level = []
+            n = len(q)
+            for i in range(n):
+                node = q.popleft()
+                level.append(node.val)
+                # res.append(node.val)
+                if node.left: 
+                    q.append(node.left)
+                    res.append(node.left.val)
+                if node.right: 
+                    q.append(node.right)
+                    res.append(node.right.val)
+            ans.append(level)
+            res.sort()
+            if len(res)>=k:
+                value = res[k-1]
+        return value
+            
 
-
+        
