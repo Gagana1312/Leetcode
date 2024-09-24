ee29a5255a718c8442b20584e9fb7f759290a757
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-09-24 16:36:43 -0600

    Time: 7323 ms (17.76%), Space: 11.8 MB (20.75%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,34 +1,42 @@
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
-        ROWS, COLS = len(board), len(board[0])
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        m,n = len(board), len(board[0])
+        w= len(word)
         path = set()
-        
-        def dfs(r,c,i):
-            if i == len(word):
+        if m==1 and n==1:
+            return board[0][0]==word
+
+        def dfs(r, c, i):
+            if i == w:
                 return True
-            if (r < 0 or c < 0 or
-                r >= ROWS or c >= COLS or
-                word[i]!= board[r][c] or 
-                (r,c) in path):
+
+            if board[r][c]!= word[i]:
+                return False
+            
+            if (r,c) in path:
                 return False
             
             path.add((r,c))
-            
-            res = (dfs(r+1,c,i+1) or
-            dfs(r-1,c,i+1) or dfs(r,c+1,i+1) or
-            dfs(r,c-1,i+1))
+
+            for i_off, j_off in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                
+                row, col = r + i_off, c + j_off
+
+                if 0 <= row < m and 0 <= col < n:
+                    if dfs(row, col, i + 1):
+                        return True
             
             path.remove((r,c))
-            return res
+            return False
         
-        for r in range(ROWS):
-            for c in range(COLS):
-                if dfs(r,c,0): return True
-        
+        for r in range(m):
+            for c in range(n):
+                if dfs(r,c,0):
+                    return True
         return False
-                
-            
-                
-            
-            
         
