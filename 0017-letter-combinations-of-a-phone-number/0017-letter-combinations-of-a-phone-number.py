d99216fffe394bc7cd877f3120120f9a32bf7192
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-09-24 13:05:09 -0600

    Time: 10 ms (86.32%), Space: 11.7 MB (71.09%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,8 +1,11 @@
-class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        res = []
-
-        digitToChar = {
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        res=[]
+        digitToChar={
             "2":"abc",
             "3":"def",
             "4":"ghi",
@@ -10,16 +13,44 @@ class Solution:
             "6":"mno",
             "7":"pqrs",
             "8":"tuv",
-            "9":"wxyz",
+            "9":"wxyz"
+            
         }
-
-        def backtrack(i, cur):
-            if len(cur) == len(digits):
+        def dfs(i,cur):
+            if len(cur)==len(digits):
                 res.append(cur)
                 return
             for c in digitToChar[digits[i]]:
-                backtrack(i+1,cur+c)
+                dfs(i+1,cur+c)
         if digits:
-            backtrack(0,"")
+            dfs(0,"")
         return res
         
+        
+        
+        
+        
+        
+        
+#         res = []
+
+#         digitToChar = {
+#             "2":"abc",
+#             "3":"def",
+#             "4":"ghi",
+#             "5":"jkl",
+#             "6":"mno",
+#             "7":"pqrs",
+#             "8":"tuv",
+#             "9":"wxyz",
+#         }
+
+#         def backtrack(i, cur):
+#             if len(cur) == len(digits):
+#                 res.append(cur)
+#                 return
+#             for c in digitToChar[digits[i]]:
+#                 backtrack(i+1,cur+c)
+#         if digits:
+#             backtrack(0,"")
+#         return res
