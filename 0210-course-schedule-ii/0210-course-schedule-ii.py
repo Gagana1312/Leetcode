f396820e96bc9c4293120da6ef6a4bf7bb91d9d3
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-09-25 14:28:16 -0600

    Time: 76 ms (40.48%), Space: 14.8 MB (34.31%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,37 +1,44 @@
-class Solution:
-    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-        # build adjacency list of prereqs
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+
         prereq = {c:[] for c in range(numCourses)}
         for crs, pre in prerequisites:
             prereq[crs].append(pre)
-        #a course has 3 possible states:
-
-
-        # visited => crs has not added to output
-
-        # visiting -> crs not added to output, but added to cycle
-
-        # unvisited -> crs not added to output or cycle
 
         output =[]
-        visit, cycle = set(), set()
+        cycle, visit= set(),set()
         def dfs(crs):
             if crs in cycle:
                 return False
             if crs in visit:
                 return True
+
             cycle.add(crs)
             for pre in prereq[crs]:
-                if dfs(pre) == False:
+                if not dfs(pre):
                     return False
+
             cycle.remove(crs)
             visit.add(crs)
             output.append(crs)
             return True
+
         for c in range(numCourses):
-            if dfs(c) == False:
+            if not dfs(c):
                 return []
-        return output
+        return output 
+
+
+            
+            
+
+            
+
 
 
         
