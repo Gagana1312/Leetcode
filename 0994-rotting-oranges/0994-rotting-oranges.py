10e1411daa588ae0803243ef7dfd054d06ddd9ae
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-09-26 14:01:14 -0600

    Time: 32 ms (61.38%), Space: 11.5 MB (79.47%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,36 +1,96 @@
-class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        q = deque()
-        time, fresh = 0, 0
+from collections import deque
+class Solution(object):
+    def orangesRotting(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        minutes=-1
+        numfresh =0
+        empty, fresh, rotten = 0,1,2
+        m,n = len(grid), len(grid[0])
+        q= deque()
 
-        ROWS, COLS = len(grid), len(grid[0])
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if grid[r][c]==1:
-                    fresh+=1
-                if grid[r][c] == 2:
-                    q.append([r,c])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == rotten:
+                    q.append((i,j))
+                elif grid[i][j]==fresh:
+                    numfresh+=1
         
-        directions=[[0,1],[0,-1],[1,0],[-1,0]]
-        while q and fresh > 0:
-            for i in range(len(q)):
-                r, c = q.popleft()
-                for dr, dc in directions:
-                    row,col = dr+r, dc+c
-                    # if in bounds and fresh, make it rotten
-                    if( row < 0 or row == len(grid) or
-                    col < 0 or col == len(grid[0]) or grid[row][col]!=1):
-                        continue
-                    
-                    grid[row][col] = 2
-                    q.append([row,col])
-                    fresh-=1
-            time+=1
-        
-        return time if fresh == 0 else -1
+        if numfresh ==0:
+            return 0
+        while q:
+            q_size= len(q)
+            minutes+=1
+            for _ in range(q_size):
+                i,j = q.popleft()
+                for r, c in [(i,j+1),(i+1,j),(i,j-1),(i-1,j)]:
+                    if 0<=r<m and 0<=c<n and grid[r][c]== fresh:
+                        grid[r][c]= rotten
+                        numfresh-=1
+                        q.append((r,c))
+
+        if numfresh==0:
+            return minutes
+        else:
+            return -1
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # m,n = len(grid), len(grid[0])
+        # visited = set()
+        # minutes =0
+
+        # def dfs((i,j),(p,q), minutes):
+        #     if i<0 or i>=m or j<0 or j>=n or grid[i][j]!='1' or grid[i][j]!='2':
+        #         return
+        #     elif grid[i][j]=='1':
+        #         grid[i][j] = '2'
+        #         minutes+=1
+        #         dfs(i+1,j, minutes)
+        #         dfs(i,j+1, minutes)
+        #         dfs(i-1,j, minutes)
+        #         dfs(i,j-1, minutes)
+        #     else:
+        #         dfs(i+1,j, minutes)
+        #         dfs(i,j+1, minutes)
+        #         dfs(i-1,j, minutes)
+        #         dfs(i,j-1, minutes)
+
+        # for i in range(m):
+        #     for j in range(n):
+        #         if grid[i][j]== '1' or grid[i][j]=='2':
+        #             dfs(i,j, minutes)
+        # return minutes
+
+
+
+            
 
         
