5985e3b2d84d62306532142903e53bb4400fc79e
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-09-26 12:56:46 -0600

    Time: 33 ms (50.65%), Space: 11.9 MB (38.02%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,27 +1,44 @@
 """
 # Definition for a Node.
-class Node:
+class Node(object):
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
 
-from typing import Optional
-class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        oldtoNew ={}
-
-        def dfs(node):
-            if node in oldtoNew:
-                return oldtoNew[node]
-            
-            clone = Node(node.val)
-            oldtoNew[node] = clone
-            for ni in node.neighbors:
-                clone.neighbors.append(dfs(ni))
-            return clone
+class Solution(object):
+    def cloneGraph(self, node):
+        """
+        :type node: Node
+        :rtype: Node
+        """
+        if not node:
+            return None
         
-        return dfs(node) if node else None
+        start = node
+        oton ={}
+        stk =[start]
+        visited=set()
+        visited.add(start)
+
+        # creating the new node
+        while stk:
+            node = stk.pop()
+            oton[node] = Node(val = node.val)
+
+            for ni in node.neighbors:
+                if ni not in visited:
+                    visited.add(ni)
+                    stk.append(ni)
+        
+        # taking the old node refernce, taking the neighbors and helping newnode to attach to its newneighbors.
+        for oldnode, newnode in oton.items():
+            for ni in oldnode.neighbors:
+                newni = oton[ni]
+                newnode.neighbors.append(newni)
+        
+        return oton[start]
+
 
 
         
