b87c834ede9f970faad525349237500c83c08efb
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-09-27 17:37:55 -0600

    Time: 342 ms (92.58%), Space: 12.8 MB (20.88%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,14 +1,51 @@
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        end = len(nums)-1
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # n= len(nums)
+        # if n==0:
+        #     return False
+        # if n==1:
+        #     return True
+        # # if n==2:
+        # #     return nums[0]==0 and nums[1]==1 or nums[0]==1
+        
+        # for i in range(n-1):
+        #     jump = nums[i]
+        #     for k in range(jump):
+        #         if jump<n and nums[i+jump] == nums[-1]:
+        #             return True
+        # return False
 
-        for i in range(len(nums)-1,-1,-1):
-            if i+nums[i]>=end:
-                end = i
+        # n= len(nums)
+        # memo = {n-1:True}
+        # def can_reach(i):
+        #     if i in memo:
+        #         return memo[i]
             
-        return True if end==0 else False
+        #     for jump in range(1, nums[i]+1):
+        #         if can_reach(i+jump):
+        #             memo[i] = True
+        #             return True
+
+        #     memo[i]= False
+        #     return False
+        # return can_reach(0)
+
+
+        n= len(nums)
+        target = n-1
+
+        for i in range(n-1,-1,-1):
+            jump = nums[i]
+            if i+jump >=target:
+                target=i
+        return target==0
 
 
 
+                
 
         
