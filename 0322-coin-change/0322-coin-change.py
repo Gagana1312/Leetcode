809c59f81f89f471bc1f277489db09cbef5e01a0
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-09-27 21:23:28 -0600

    Time: 1090 ms (19.69%), Space: 31.8 MB (8.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,14 +1,41 @@
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        #DP bottom up DP[0]=0 DP[1]=1 others are greater than 1, DP[2]=1+1 rest are >2
-      # DP[3]=1 DP[4]=1 DP[5]=1 DP[6]=2 so DP[7]=1+DP[6]=1+2=3 
-        dp = [amount+1]*(amount+1) 
-        dp[0]=0
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        # coins.sort()
+        # count=0
+        # for n in coins[::-1]:
+        #     while amount >=n:
+        #         amount -=  n
+        #         count+=1
+        # if amount ==0:
+        #     return count
+        # else:
+        #     return -1
 
-        for a in range(1, amount+1):
-            for c in coins:
-                if a-c>=0:
-                    dp[a]=min(dp[a],1+dp[a-c])
-        return dp[amount] if dp[amount]!= amount+1 else -1
+        coins.sort()
+        memo = {0:0}
+
+        def min_coins(amt):
+            if amt in memo:
+                return memo[amt]
+            minn = float('inf')
+            for coin in coins:
+                diff= amt - coin
+                if diff<0:
+                    break
+                minn = min(minn, 1+min_coins(diff))
+            memo[amt] = minn
+                
+            return minn
+        result =min_coins(amount)
+        if result <float('inf'):
+            return result
+        else:
+            return -1
+        
 
         
