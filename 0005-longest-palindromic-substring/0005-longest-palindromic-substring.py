295791ad0e4b83791feb4601964ce8bf9ff6da9e
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-10-03 14:16:27 -0600

    Time: 451 ms (45.72%), Space: 16.6 MB (75.52%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,29 +1,28 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        sol = ''
-     
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
 
-        for i in range(len(s)):
-            #odd length
-            l, r =i, i
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if len(s[l:r+1]) >= len(sol):
-                    sol = s[l: r+1]
+        n = len(s)
+        res=""
+        reslen=0
+
+        for i in range(n):
+            # odd length
+            l,r =i,i
+            while l>=0 and r<n and s[l]==s[r]:
+                if len(s[l:r+1]) >= reslen:
+                    res = s[l:r+1]
+                    reslen= r-l+1
                 l-=1
                 r+=1
-            
-            #odd lenght
+            #even length
             l,r=i,i+1
-            while l>=0 and r<len(s) and s[l]==s[r]:
-                if len(s[l:r+1])>=len(sol):
-                    sol=s[l:r+1]
+            while l>=0 and r<n and s[l]==s[r]:
+                if len(s[l:r+1])>=reslen:
+                    res = s[l:r+1]
+                    reslen= r-l+1
                 l-=1
                 r+=1
-        return sol
+        return res
 
-
+            
         
