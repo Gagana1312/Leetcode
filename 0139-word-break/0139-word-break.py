fafda5db2334827f16d691aee7f4fc277d3dc2f6
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-10-03 23:41:37 -0600

    Time: 16 ms (85.76%), Space: 11.8 MB (76.10%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,27 +1,23 @@
-class Solution:
-    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
-        # visited = set()
-
-        # for word in wordDict:
-        #     if word in s and word not in visited:
-        #         visited.add(word)
-        #         return True
-        #     if word in s and word in visited:
-        #         return False
-
-        dp=[False]*(len(s)+1)
-        dp[len(s)]=True
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        """
+        :type s: str
+        :type wordDict: List[str]
+        :rtype: bool
+        """
+        dp = [False]*(len(s)+1)
+        dp[len(s)] = True
 
         for i in range(len(s)-1,-1,-1):
             for word in wordDict:
-                if(i+len(word)) <= len(s) and s[i:i+len(word)] == word:
-                    dp[i] = dp[i + len(word)]
+                if (i+len(word)) <= len(s) and s[i:i+len(word)] == word:
+                    dp[i] = dp[i+len(word)]
                 if dp[i]:
                     break
         return dp[0]
+
+
         
                 
-
-                
+            
         
