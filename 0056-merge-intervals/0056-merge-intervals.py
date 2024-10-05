9824409bb24d26dbe47328161909cf0b2061b366
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-10-05 12:40:39 -0600

    Time: 89 ms (97.45%), Space: 16.5 MB (97.25%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,32 +1,76 @@
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # for i in range(len(intervals)-2):
-        #     if intervals[i][1]>intervals[i+1][0]:
-        #         intervals[i]=[intervals[i][0],intervals[i+1][1]]
-        #         intervals.remove(intervals[i+1])
-        # return intervals
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        # res =[]
+        # intervals = sorted(intervals, key=lambda x: x[0])
+        # i=0
+        # while i < len(intervals):
+            
+        #     if i<len(intervals)-1  and intervals[i][0]<=intervals[i+1][0] and intervals[i][1]>=intervals[i+1][0] and intervals[i][1]<=intervals[i+1][1]:
+        #         res.append([intervals[i][0], intervals[i+1][1]])
+        #         i+=1
+        #     elif i<len(intervals)-1 and intervals[i][1]<=intervals[i+1][0]:
+        #         res.append(intervals[i])
+        #         res.append(intervals[i+1])
+        #         i+=1
+        #     else:
+        #         res.append(intervals[i])
+        #     i+=1
+        # return res
 
-        #O(nlogn)
 
-        intervals.sort(key=lambda i : i[0])
-        res =[intervals[0]]
 
-        for start,end in intervals[1:]:
-            prev_end = res[-1][1]
-            if start <= prev_end:
-                res[-1][1] = max(prev_end,end)
+        merge = []
+        intervals.sort(key = lambda interval: interval[0])
+
+        for interval in intervals:
+            if not merge or merge[-1][1]<interval[0]:
+                merge.append(interval)
             else:
-                res.append([start,end])
-
-        return res
-
+                merge[-1] = [merge[-1][0], max(merge[-1][1], interval[1])]
+        return merge
 
 
-
-
-
-
+        # for i in intervals:
+        #     if not res or res[-1][1]<i[0]:
+        #         res.append(i)
+        #     else:
+        #         res[-1] = [res[-1][0], max(res[-1][1],i[1])]
+        # return res
         
+        
+        
+        # res = []
+        # if not intervals:
+        #     return res
+        # if len(intervals) == 1:
+        #     return intervals
+        # intervals = sorted(intervals, key=lambda x: x[0])
+
+        # i = 0
+        # while i < len(intervals):
+        #     current_start, current_end = intervals[i]
+        #     while i < len(intervals) - 1 and intervals[i + 1][0] <= current_end:
+        #         current_end = max(current_end, intervals[i + 1][1])  
+        #         i += 1
+        #     res.append([current_start, current_end])
+        #     i += 1
+        # return res
+            
+
+
+
+
+
+
+
+
+
+
+                
 
 
         
