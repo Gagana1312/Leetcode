4cdad1c980b9eaef21f792f78b77830781b3197b
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-11-01 09:15:58 -0600

    Time: 307 ms (61.26%), Space: 18.4 MB (73.10%) - LeetHub

---
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
@@ -0,0 +1,15 @@
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        # Initialize an empty result list
+        result = ""
+
+        for char in s:
+            # If the last two characters in result are the same as the current character, skip this character
+            if len(result) >= 2 and result[-1] == char and result[-2] == char:
+                continue
+            # Otherwise, add the character to the result
+            result+=char
+
+        # Join the result list to form the final string and return it
+        return result
+        
