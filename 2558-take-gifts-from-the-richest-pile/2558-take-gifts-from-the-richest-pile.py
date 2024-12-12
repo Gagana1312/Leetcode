6f65bd61d93f83390aa1a56ceae06e6c17e7deee
Gagana Ananda <116911605+Gagana1312@users.noreply.github.com>
2024-12-11 21:20:15 -0700

    Time: 5 ms (100.00%), Space: 12.4 MB (20.55%) - LeetHub

--- 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
+++ 2558-take-gifts-from-the-richest-pile/2558-take-gifts-from-the-richest-pile.py
@@ -5,8 +5,19 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        gifts.sort(reverse=True)
-        for i in range(k):
-            gifts[0] = int(gifts[0] ** 0.5)
-            gifts.sort(reverse=True)
-        return sum(gifts)
+        # gifts.sort(reverse=True)
+        # for i in range(k):
+        #     gifts[0] = int(gifts[0] ** 0.5)
+        #     gifts.sort(reverse=True)
+        # return sum(gifts)
+        
+        max_heap = [-g for g in gifts]
+        heapq.heapify(max_heap)
+
+        while k > 0 and max_heap:
+            largest = -heapq.heappop(max_heap)
+            reduced = math.floor(largest ** 0.5)
+            heapq.heappush(max_heap, -reduced)
+            k -= 1
+
+        return int(-sum(max_heap))
